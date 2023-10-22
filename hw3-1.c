5cafc43d74bbede527295a8a1141ac49e79eb828
112AB0059 <146065279+112AB0059@users.noreply.github.com>
2023-10-22 17:54:22 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// 函數用於判斷一個數是否為質數
 bool isPrime(int n) {
     if (n <= 1) {
-        return false; // 1和所有負數都不是質數
+        return false;
     }
     for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
-            return false; // 如果有除了1和自身以外的因數，則不是質數
+            return false;
         }
     }
-    return true; // 沒有其他因數，是質數
+    return true;
 }
 
 int main() {
     int num;
 
-    // 提示用戶輸入一個正整數
     printf("請輸入一個正整數：");
     scanf("%d", &num);
 
-    // 判斷是否為質數並顯示結果
     if (isPrime(num)) {
         printf("YES\n");
     } else {
